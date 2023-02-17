41acf530ffc92b30fdf27a7825c3a5e89ee33a9a
Sergey Kulanin <sjulanin1981@gmail.com>
2023-02-17 13:29:18 +0300

    Combining all folders

---
+++ Example5_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "mary")
+{
+    Console.WriteLine("Wow, it is Mary!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
